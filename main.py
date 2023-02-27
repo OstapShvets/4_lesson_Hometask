1206736e5228ffd6c31ee44d28be6033202f0ccf
OstapShvets <shvetsostap200@gmail.com>
2023-02-28 00:22:49 +0200

    дз

---
+++ main.py
@@ -0,0 +1,31 @@
+########################1 завдання####################################
+#new_value =int(input("Type a number:"))
+#result  =  new_value / 2 if  new_value < 100 else -new_value
+#print(result)
+
+############ 2 завдання
+
+#new_value =int(input("Type a number:"))
+#mykola = new_value ** 0 if new_value < 100  else 0
+#print(mykola)
+
+
+######### 3 завдання ##########
+
+#new_value =int(input("Type a number:"))
+#lesson = True if new_value < 100  else False
+#print(lesson)
+
+########### 4 завдання ###########
+
+#my_str = str(input("Type a word:"))
+#result = my_str * 2 if len(my_str) < 5 else my_str
+#print(result)
+
+
+
+######## 5 завдання ###########
+
+my_str = str(input("Type a word:"))
+result = my_str + my_str[::-1] if len(my_str) < 5 else my_str
+print(result)
